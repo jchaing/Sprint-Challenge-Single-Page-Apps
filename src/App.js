1e6846208e8749c83fb2c387642b6aae94c7c2cd
john <john@spinningplates.org>
2019-10-11 18:34:56 -0700

    Nav component with NavLinks to click to routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Header from './components/Header.js';
+import Nav from './components/Nav';
 import WelcomePage from './components/WelcomePage';
 import CharacterList from './components/CharacterList';
 
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <main>
       <Header />
+      <Nav />
       <Route exact path="/" component={WelcomePage} />
       <Route path="/characters" component={CharacterList} />
     </main>
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function Nav() {
+  return (
+    <div>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/characters">Characters</NavLink>
+    </div>
+  );
+}
