60ac1858f6a891b11594f5b4db04de4cf9fdd564
john <john@spinningplates.org>
2019-10-11 18:09:29 -0700

    Route to WelcomePage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 import Header from "./components/Header.js";
+import WelcomePage from './components/WelcomePage'
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/" component={WelcomePage} />
     </main>
   );
 }
