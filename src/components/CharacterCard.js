d6809346dc1f5af663dc71ee41b521129a250fec
john <john@spinningplates.org>
2019-10-11 18:28:00 -0700

    Hook in API to CharacterList, pass state props to CharacterChard and render

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React from 'react';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({
+  image,
+  name,
+  status,
+  species,
+  origin,
+  location
+}) {
+  return (
+    <div>
+      <div>
+        <img src={image} alt="" />
+      </div>
+      <div>
+        <h2>{name}</h2>
+        <p>Status: {status}</p>
+        <p>Species: {species}</p>
+        <p>Origin: {origin}</p>
+        <p>Location: {location}</p>
+      </div>
+    </div>
+  );
 }
